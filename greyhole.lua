d481e113cc3966a64f290c91bcab01463d0cfc76
tlubke <38451140+tlubke@users.noreply.github.com>
2019-12-22 21:24:57 -0600

    floating point string format
    More specific control of padded and trailing zeroes, with a min and max decimal place string conversion. Will round up if the most significant point after the max is >= 5.

--- greyhole.lua
+++ greyhole.lua
@@ -133,6 +133,7 @@ function redraw()
   screen.aa(0)
   screen.level(15)
   screen.move(5, 15)
+  screen.font_face(1)
   screen.font_size(16)
   screen.text("greyhole")
   screen.move(75, 15)
@@ -145,28 +146,84 @@ function redraw()
   screen.move(8, 28)
   screen.text("time:")
   screen.move(120, 28)
-  screen.text_right(params:get("time"))
+  screen.font_face(2)
+  screen.text_right(format_float(params:get("time"), 2, 2))
+  screen.font_face(1)
 
   screen.move(8, 36)
   screen.text("size:  ")
   screen.move(120, 36)
-  screen.text_right(params:get("size"))
+  screen.font_face(2)
+  screen.text_right(format_float(params:get("size"), 2, 2))
+  screen.font_face(1)
 
   screen.move(8, 44)
   screen.text("damp:  ")
   screen.move(120, 44)
-  screen.text_right(params:get("damp"))
-
+  screen.font_face(2)
+  screen.text_right(format_float(params:get("damp"), 2, 2))
+  screen.font_face(1)
+  
   screen.level(alt and 10 or 2)
   screen.move(8, 52)
   screen.text("diff:  ")
   screen.move(120, 52)
-  screen.text_right(params:get("diff"))
+  screen.font_face(2)
+  screen.text_right(format_float(params:get("diff"), 2, 2))
+  screen.font_face(1)
 
   screen.move(8, 60)
   screen.text("fdbk:  ")
   screen.move(120, 60)
-  screen.text_right(params:get("feedback"))
+  screen.font_face(2)
+  screen.text_right(format_float(params:get("feedback"), 2, 2))
 
   screen.update()
+end
+
+function format_float(num, min_places, max_places)
+  -- TODO: exception for min > max
+  local buffer = {}
+  local s = tostring(num)
+  local point_index = string.find(s, "%.")
+  
+  -- find index of '.' in floating point
+  -- if an int, just return a string of num
+  if point_index == nil then
+    return s
+  end
+  
+  digits_after_point = string.sub(s, point_index + 1, point_index + max_places + 1)
+  
+  if #digits_after_point > max_places then
+    -- float has more precision than max_places, ROUND UP if neccessary
+    for i = 1, #digits_after_point do
+      -- put all trailing zeroes in string buffer
+      if string.sub(digits_after_point, i, i) == '0' then
+        table.insert(buffer, '0')
+      else
+        -- first non-zero character at ith index after .
+        first_nz = i
+        break
+      end
+    end
+    
+    round_digit = string.sub(digits_after_point, #digits_after_point, #digits_after_point)
+    
+    if round_digit >= '5' then
+      digits_after_zeroes = string.sub(digits_after_point, first_nz, #digits_after_point) + 5
+    else
+      digits_after_zeroes = string.sub(digits_after_point, first_nz, #digits_after_point)
+    end
+    
+    -- truncate the rounding digit and floating point if string was converted to a num
+    table.insert(buffer, string.sub(digits_after_zeroes, 1, #digits_after_point - first_nz))
+  else
+    -- float has less than max_places, no rounding neccessary
+    after_point = string.sub(s, point_index + 1, point_index + max_places)
+    table.insert(buffer, after_point)
+    table.insert(buffer, string.rep("0", min_places - string.len(after_point)) )
+  end
+  
+  return (string.sub(s, 0, point_index) .. table.concat(buffer))
 end
